5e3a27bc76e36b2f9c5e0050b2d422210a537111
SpyrosSiachamis <csd5503@csd.uoc.gr>
2026-05-20 03:51:56 +0300

    Login Page main layout completed, need to add aside inner content and login logic

--- HY-351-PROJECT-2026/frontend/pages/index.tsx
+++ HY-351-PROJECT-2026/frontend/pages/index.tsx
@@ -1,5 +1,27 @@
 import Head from "next/head";
-import Link from "next/link";
+import styles from "@/styles/Home.module.css"
+
+export function AsideColumn() {
+  return(
+    <>
+      <aside className={styles.asidesection}>
+        <div className={styles.welcome}>
+          <h1 className={styles.welcometext1}>Welcome To Heraklion</h1>
+          <h2 className={styles.welcometext2}>Please Sign in to your account</h2>
+        </div>
+      </aside>
+    </>
+  );
+}
+
+export function Main() {
+  return (
+    <>
+      <div className={styles.mainsection}>
+      </div>
+    </>
+  );
+}
 
 export default function Home() {
   return (
@@ -7,11 +29,11 @@ export default function Home() {
       <Head>
         <title>HY351 APP</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head> 
-      <div>
-        <h1>Home</h1>
-        <Link href="/events">Go To Events Page</Link>
+        <link rel="icon" href="/logo.svg" />
+      </Head>
+      <div className={styles.maincontainer}>
+        <AsideColumn />
+        <Main />
       </div>
     </>
   );
