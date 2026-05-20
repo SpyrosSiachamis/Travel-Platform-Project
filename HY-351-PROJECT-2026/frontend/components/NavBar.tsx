faae8d15a55d0d1eb2d0dc46ec9f2864aad3d143
SpyrosSiachamis <csd5503@csd.uoc.gr>
2026-05-21 01:36:40 +0300

    Added states for the navbar based on user role

--- HY-351-PROJECT-2026/frontend/components/NavBar.tsx
+++ HY-351-PROJECT-2026/frontend/components/NavBar.tsx
@@ -1,4 +1,5 @@
-// import { useState, useEffect } from 'react';
+/* eslint-disable indent */
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 // import { useRouter } from 'next/router';
 import styles from '../styles/NavBar.module.css'
@@ -7,37 +8,86 @@ type NavItem = {
     path: string;
 };
 
-export function NavBtn(navItem: NavItem) {
+export function NavBtn({ label, path }: NavItem) {
     return (
         <>
-            <Link href={navItem.path} className={styles.navbtn}>{navItem.label}</Link>
+            <Link href={path} className={styles.navbtn}>{label}</Link>
         </>
     );
 }
+type UserState = {
+    isLoggedIn: boolean;
+    role: string;
+    username?: string;
+}
 
 export default function NavBar() {
-    // const [user, setUser] = useState<UserState>({
-    //     isLoggedIn: false,
-    //     role: 'Guest',
-    //     username: null
-    // });
-    return(
-        <>
-            <header className={styles.navbar}>
-                <div className={styles.leftside}>
-                    <div className={styles.logo}></div>
-                    <div className={styles.title}>Travel<br></br>Heraklion</div>
-                    <nav className={styles.leftbtns}>
-                        <NavBtn label='Home' path='/' />
-                        <NavBtn label='Events' path='/events' />
+    const [user, setUser] = useState<UserState>({
+        isLoggedIn: false,
+        role: 'Guest',
+    });
+    if (user.role === 'Guest') {
+        return (
+            <>
+                <header className={styles.navbar}>
+                    <div className={styles.leftside}>
+                        <div className={styles.logo}></div>
+                        <div className={styles.title}>Travel<br></br>Heraklion</div>
+                        <nav className={styles.leftbtns}>
+                        </nav>
+                    </div>
+                    <nav className={styles.rightbtns}>
                     </nav>
-                </div>
-                <nav className={styles.rightbtns}>
-                    {/* <NavBtn label='Favourites' path='/' />
-                    <NavBtn label='History' path='/' />
-                    <NavBtn label='Favourites' path='/' /> */}
-                </nav>
-            </header>
-        </>
-    );
+                </header>
+            </>
+        );
+    }
+    if (user.role === 'excursionist') {
+        return (
+            <>
+                <header className={styles.navbar}>
+                    <div className={styles.leftside}>
+                        <div className={styles.logo}></div>
+                        <div className={styles.title}>Travel<br></br>Heraklion</div>
+                        <nav className={styles.leftbtns}>
+                            <NavBtn label='Home' path='/events' />
+                            <NavBtn label='Events' path='/events' />
+                        </nav>
+                    </div>
+                    <nav className={styles.rightbtns}>
+                        <NavBtn label='Favourites' path='/' />
+                        <NavBtn label='History' path='/' />
+                        <NavBtn label='Notifications' path='/' />
+                        <NavBtn label='Profile' path='/' />
+                        <NavBtn label='Settings' path='/' />
+                    </nav>
+                </header>
+            </>
+        );
+    }
+    if(user.role ==='trip_creator'){
+        return (
+            <>
+                <header className={styles.navbar}>
+                    <div className={styles.leftside}>
+                        <div className={styles.logo}></div>
+                        <div className={styles.title}>Travel<br></br>Heraklion</div>
+                        <nav className={styles.leftbtns}>
+                            <NavBtn label='Home' path='/events' />
+                            <NavBtn label='Events' path='/events' />
+                        </nav>
+                    </div>
+                    <nav className={styles.rightbtns}>
+                        <NavBtn label='Favourites' path='/' />
+                        <NavBtn label='History' path='/' />
+                        <NavBtn label='Notifications' path='/' />
+                        <NavBtn label='Add Event' path='/' />
+                        <NavBtn label='Profile' path='/' />
+                        <NavBtn label='Settings' path='/' />
+                    </nav>
+                </header>
+            </>
+        );
+    }
+    return null;
 }
