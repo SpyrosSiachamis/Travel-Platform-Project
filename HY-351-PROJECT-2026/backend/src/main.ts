d043b20fea30f34d3a259e275d96f661ffcc2d96
SpyrosSiachamis <csd5503@csd.uoc.gr>
2026-05-21 04:26:06 +0300

    added session cookie settings

--- HY-351-PROJECT-2026/backend/src/main.ts
+++ HY-351-PROJECT-2026/backend/src/main.ts
@@ -6,18 +6,23 @@ import session = require('express-session');
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: ['http://localhost:3000'],
-    credentials: true // To allow cookies to pass across backend and frontend port
-  });
-
   app.use(
     session({
       secret: 'my-secret',
       resave: false,
       saveUninitialized: false,
+      cookie: {
+        secure: false,
+        sameSite: 'lax',
+      }
     }),
   );
+
+  app.enableCors({
+    origin: ['http://localhost:3000'],
+    credentials: true // To allow cookies to pass across backend and frontend port
+  });
+
 
   await app.listen(5000);
 }
