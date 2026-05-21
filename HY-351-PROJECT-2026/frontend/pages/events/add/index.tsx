d64f9ecbcc45641469a6746ed80f4752fadbadb8
SpyrosSiachamis <csd5503@csd.uoc.gr>
2026-05-22 02:22:47 +0300

    Began add event page. Added grid with inputs

--- HY-351-PROJECT-2026/frontend/pages/events/add/index.tsx
+++ HY-351-PROJECT-2026/frontend/pages/events/add/index.tsx
@@ -1,8 +1,85 @@
-export default function addEventPage()
-{
-    return(
+/* eslint-disable indent */
+import styles from '@/styles/addEvent.module.css'
+
+type InputType = {
+    id: string;
+    text: string;
+    iType: string;
+};
+
+export function InputField(type: InputType) {
+    return (
         <>
-        <h1>Hello World!</h1>
+            <div className={styles.inputdiv}>
+                <label htmlFor={type.id} className={styles.inputlabel}>{type.text}:</label>
+                <input id={type.id} name={type.id} type={type.iType} placeholder={type.text} className={styles.inputfield}></input>
+            </div>
+        </>
+    );
+}
+
+export function AddEventBtn()
+{
+    return (
+        <input type="submit" value="Add Event" className={styles.addeventbtn}/>
+    )
+}
+
+export function DescriptionField() {
+    return (
+        <>
+        <label htmlFor='description' id='descriptionLabel' className={styles.descriptionlabel}>Description</label>
+            <textarea
+                id="description"
+                name="description"
+                rows={13}
+                cols={18}
+                placeholder='Enter event description'
+                className={styles.descriptionfield}
+            /> 
+        </>
+    );
+}
+
+export function AddEventForm() {
+    return (
+        <div className={styles.addeventdiv}>
+            <div className={styles.addeventform}>
+                <h1 className={styles.formtitle}>Create a new Event</h1>
+                <form className={styles.formcontainer}>
+                    <div className={styles.inputfields}>
+                        <div className={styles.griditem}>
+                            <InputField id="eventtitle" iType="text" text="Event Title" />
+                            <InputField id="eventlocation" iType="text" text="Location" />
+                            <InputField id="eventprice" iType="text" text="Price" />
+                            <DescriptionField />
+
+                        </div>
+                        <div className={styles.griditem}>
+                            <InputField id="eventtype" iType="text" text="Choose Event Type" />
+                            <InputField id="eventparticipants" iType="text" text="Maximum Participants" />
+                            <InputField id="eventschedule" iType="text" text="Schedule" />
+
+                        </div>
+                    </div>
+                    <div className={styles.addeventcontainer}>
+                        <AddEventBtn />
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
+
+
+
+export default function AddEventPage() {
+    return (
+        <>
+            <main className={styles.mainsection}>
+                <div className={styles.backgrounddiv}></div>
+                <AddEventForm />
+            </main>
         </>
     );
 }
