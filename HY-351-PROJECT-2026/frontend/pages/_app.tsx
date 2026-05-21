31f6ba54312c5cfa47a743fb38de6ef3ad653173
SpyrosSiachamis <csd5503@csd.uoc.gr>
2026-05-21 04:25:28 +0300

    Added global state handler

--- HY-351-PROJECT-2026/frontend/pages/_app.tsx
+++ HY-351-PROJECT-2026/frontend/pages/_app.tsx
@@ -1,12 +1,16 @@
+/* eslint-disable indent */
 import NavBar from "@/components/NavBar";
+import { AuthProvider } from "@/context/authContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <NavBar />
-      <Component {...pageProps} />
+      <AuthProvider>
+        <NavBar />
+        <Component {...pageProps} />
+      </AuthProvider >
     </>
   )
 }
